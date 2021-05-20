abdc4d28ca84a0b40840080ef4e7b078d75ecf29
leonhardt <jacob.leonhardt649@gmail.com>
2021-05-20 13:09:57 -0500

    get session setup

--- authenticate-me/backend/routes/api/session.js
+++ authenticate-me/backend/routes/api/session.js
@@ -40,3 +40,17 @@ router.delete(
   );
 
   module.exports = router;
+
+// Restore session user
+router.get(
+    '/',
+    restoreUser,
+    (req, res) => {
+      const { user } = req;
+      if (user) {
+        return res.json({
+          user: user.toSafeObject()
+        });
+      } else return res.json({});
+    }
+  );
