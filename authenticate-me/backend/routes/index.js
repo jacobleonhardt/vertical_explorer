4cd1b3b0c2343dedf0256c0fc7660203a35b5fa6
leonhardt <jacob.leonhardt649@gmail.com>
2021-05-20 12:47:12 -0500

    user login setup

--- authenticate-me/backend/routes/index.js
+++ authenticate-me/backend/routes/index.js
@@ -2,8 +2,15 @@
 const express = require('express');
 const apiRouter = require('./api');
 const router = express.Router();
+const sessionRouter = require('./api/session');
+const usersRouter = require('./api/users');
 
+// Set Routes
 router.use('/api', apiRouter);
+router.use('/session', sessionRouter);
+router.use('/users', usersRouter);
+
+// HTTP Methods
 router.get('/hello/world', function(req, res) {
   res.cookie('XSRF-TOKEN', req.csrfToken());
   res.send('Hello World!');
