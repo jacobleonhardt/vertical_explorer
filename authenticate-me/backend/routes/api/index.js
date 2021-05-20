73ee55ba2cb75781f6f04f26294e36229a190e2e
leonhardt <jacob.leonhardt649@gmail.com>
2021-05-19 22:50:20 -0500

    api route setup

---
+++ authenticate-me/backend/routes/api/index.js
@@ -0,0 +1,9 @@
+// backend/routes/api/index.js
+const router = require('express').Router();
+
+router.post('/test', function(req, res) {
+    res.json({ requestBody: req.body });
+  });
+
+
+module.exports = router;
--- authenticate-me/backend/routes/index.js
+++ authenticate-me/backend/routes/index.js
@@ -1,7 +1,9 @@
 // backend/routes/index.js
 const express = require('express');
+const apiRouter = require('./api');
 const router = express.Router();
 
+router.use('/api', apiRouter);
 router.get('/hello/world', function(req, res) {
   res.cookie('XSRF-TOKEN', req.csrfToken());
   res.send('Hello World!');
